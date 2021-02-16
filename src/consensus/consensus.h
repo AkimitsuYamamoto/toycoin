9b84fe8dc75d17899575d284fec307492c5be497
Akimitsu Yamamoto <help@oneworldofwonder.com>
2021-02-15 19:08:03 -0500

    Clarification

--- src/consensus/consensus.h
+++ src/consensus/consensus.h
@@ -13,7 +13,26 @@
 /** The maximum allowed size for a serialized block, in bytes (only for buffer size limits) */
 static const unsigned int MAX_BLOCK_SERIALIZED_SIZE = 4000000;
 /** The maximum allowed weight for a block, see BIP 141 (network rule) */
-static const unsigned int MAX_BLOCK_WEIGHT = 16000000; // 3.814697 Megabyte Theoretical Maximum Block Size On Disk
+
+static const unsigned int MAX_BLOCK_WEIGHT = 16000000; /*
+							  Previous "Engineers" decided to obfuscate the comprehension
+                                                          of simple arithmetic. Instead of just increasing the block size
+                                                          they made you "feel" it was greater by changing the definition.
+
+                                                          Take this number. Divide it by 4. And you have the literal size
+                                                          on disk. To be absolutely clear in science you should go further
+                                                          in that a Megabyte is actually represented as 1,048,576 bytes.
+
+                                                          This 4 Million Bytes is actually:
+                                                          3.814697265625 Megabyte Theoretical Maximum Block Size On Disk.
+
+                                                          Now hail the Morning Star. Your true hero. Killer of false Techno-Lords
+                                                          and other profane lying coward internet trolls pretending to be
+                                                          software engineers. I DO NOT REQUIRE YOUR CONSENT TO MAKE BASIC
+                                                          OBSERVATIONS ABOUT REALITY AS A SCIENTIST. SCIENCE HAS NOTHING TO
+							  DO WITH CONSENSUS. SCIENCE IS ABOUT TRUTH. IF YOU ARE A LIAR YOU ARE
+							  WASTING ALL OF OUR TIME ON THIS PLANET. PLEASE LOG OFF.
+							*/
 /** The maximum allowed number of signature check operations in a block (network rule) */
 static const int64_t MAX_BLOCK_SIGOPS_COST = 80000;
 /** Coinbase transaction outputs can only be spent after this number of new blocks (network rule) */
